1ac455e2ee5cdc17eb408a2df15cc41a2c273092
dnjyj1024 <dnjyj1024@naver.com>
2022-09-27 17:02:19 +0900

    04

--- main.c
+++ main.c
@@ -4,14 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a,b,c;
+	int a;
 	
-	printf("input the swcond:");
+	printf("input the year:");
 	scanf("%i", &a);
-	b=a/60;
-	c=a%60;
-
-	printf("the time is %i : %i\n",b,c);
+	
+	if((a%4 == 0&&a%100 != 0) || a%400 == 0)
+		printf("%i is the leap year\n",a);
+	else
+		printf("%i is not the leap year\n",a);
 	
 	return 0;
 }
